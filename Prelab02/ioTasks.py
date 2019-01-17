005f0c9d86e65ec5aaecb651f9a087ad450f4c13
ee364g02 <mohdshah@purdue.edu>
2019-01-17 01:25:04 -0500

    done~

--- Prelab02/ioTasks.py
+++ Prelab02/ioTasks.py
@@ -10,7 +10,7 @@ import sys     # Each  one on a line
 # Module  level  Variables. (Write  this  statement  verbatim .)
 #######################################################
 DataPath = os.path.expanduser("~ee364/DataFolder/Prelab02")
-
+#------------------------------------------------------------------problem 1--------------------------------------------
 def getMaxDifference(symbol: str) -> str:
     filename = symbol.__add__('.dat')
     DataText = os.path.join(DataPath, filename)
@@ -31,7 +31,7 @@ def getMaxDifference(symbol: str) -> str:
             date = str(newData[0])
 
     return date
-
+#-------------------------------------------problem 2-------------------------------------------------------------------
 def getGainPercent(symbol: str):
     filename = symbol.__add__('.dat')
     DataText = os.path.join(DataPath, filename)
@@ -51,7 +51,7 @@ def getGainPercent(symbol: str):
     percent = round((day/(len(rawData) - 2)) * 100, 4)
 
     return percent
-
+#-------------------------------------------------------problem 3-------------------------------------------------------
 def getVolumeSum(symbol: str, date1: str, date2: str) -> int:
     if date1 >= date2:
         return None
@@ -72,7 +72,7 @@ def getVolumeSum(symbol: str, date1: str, date2: str) -> int:
     total = int(round(sumVolume))
 
     return total
-
+#----------------------------------------------------problem 4----------------------------------------------------------
 def getBestGain(date: str):
     filename = ['AAPL.dat', 'AMZN.dat', 'FB.dat', 'MSFT.dat', 'TSLA.dat']
     maxGain = 0
@@ -91,21 +91,51 @@ def getBestGain(date: str):
                     maxGain = round(gain, 4)
 
     return maxGain
-
-def getAveragePrice(symbol: str, year):
+#-----------------------------------------------------------------problem 5---------------------------------------------
+def getAveragePrice(symbol: str, year: int):
     filename = symbol.__add__('.dat')
     DataText = os.path.join(DataPath, filename)
     with open(DataText) as f:
-        raw = f.readlines()  # read and return line in files seperately
+        rawData = f.readlines()  # read and return line in files seperately
 
-def getCountOver(symbol: str, price):
+    avg = 0
+
+    for i in range(2, len(rawData)):
+        newData = str(rawData[i]).split(',')
+        date = newData[0].split('/')
+        if str(year) in date[0]:
+            closeVal = float(newData[1])
+            openVal = float(newData[3])
+            val = (closeVal + openVal) / 2
+            avg = avg + val
+
+    return round(avg, 4)
+#-----------------------------------------------------------problem 6---------------------------------------------------
+def getCountOver(symbol: str, price: float):
     filename = symbol.__add__('.dat')
     DataText = os.path.join(DataPath, filename)
     with open(DataText) as f:
-        raw = f.readlines()  # read and return line in files seperately
+        rawData = f.readlines()  # read and return line in files seperately
+
+    days = 0
+    tmp = [ ]
+
+    for i in range(2, len(rawData)):
+        tmp = [ ]
+        newData = str(rawData[i]).split(',')
+        p = newData[5].split('\n')
+        z = p.pop()  # to remove the '\n'
+        highPrice = float(p.pop())  # get the list as string
+        for j in range(1, len(newData) - 1):
+            if j != 2:
+                tmp.append(float(newData[j]))
+        tmp.append(highPrice)
+        if all(val >= price for val in tmp):
+            days = days + 1
+
+    return days
 
 # This  block  is  optional
 if __name__  == "__main__":
 # Write  anything  here to test  your  code.
-   p = getBestGain('2019/01/11')
-   #print(p)
+   p = getCountOver('AAPL', 100.00)
