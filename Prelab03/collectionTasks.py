63e4276441ce31a0af3804df950daa930d13f692
ee364g02 <mohdshah@purdue.edu>
2019-01-24 12:21:44 -0500

    starting

---
+++ Prelab03/collectionTasks.py
@@ -0,0 +1,49 @@
+#######################################################
+#    Author: Nur Nadhira Aqilah Binti Mohd Shah
+#    email: mohdshah@purdue.edu
+#    ID: mohdshah
+#    Date: 1/24
+#######################################################
+import os      # List of  module  import  statements
+import sys     # Each  one on a line
+
+# Module  level  Variables. (Write  this  statement  verbatim .)
+#######################################################
+DataPath = os.path.expanduser("~ee364/DataFolder/Prelab03")
+
+def getComponentCountByProject(projectID, componentSymbol):
+    pass
+
+def getComponentByStudent(studentName, componentSymbol):
+    pass
+
+def getParticipationByStudent(studentName):
+    pass
+
+def getParticipationByProject(projectID):
+    pass
+
+def getCostOfProjects():
+    pass
+
+def getProjectByComponent(componentIDs):
+    pass
+
+def getCommonByProject(projectID1, projectID2):
+    pass
+
+def getComponentReport(componentIDs):
+    pass
+
+def getCircuitByStudent(studentNames):
+    pass
+
+def getCircuitByComponent(componentIDs):
+    pass
+
+
+
+# This  block  is  optional
+if __name__  == "__main__":
+# Write  anything  here to test  your  code.
+    ...
