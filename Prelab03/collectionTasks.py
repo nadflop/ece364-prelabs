cb5943287091298d17b2dbdcead9701d1f2e7644
ee364g02 <mohdshah@purdue.edu>
2019-01-27 00:35:53 -0500

    starting

--- Prelab03/collectionTasks.py
+++ Prelab03/collectionTasks.py
@@ -11,8 +11,56 @@ import sys     # Each  one on a line
 #######################################################
 DataPath = os.path.expanduser("~ee364/DataFolder/Prelab03")
 
-def getComponentCountByProject(projectID, componentSymbol):
-    pass
+#-----------------------------------problem 1--------------------------------------------
+def getComponentCountByProject(projectID: str, componentSymbol: str) -> int:
+    DataFile = os.path.join(DataPath, 'maps')
+    DataText = os.path.join(DataFile, 'projects.dat') #open projects file
+    with open(DataText) as f:
+        rawData = f.readlines()
+
+    compMap = {'R': "resistors.dat",
+               'I': "inductors.dat",
+               'C': "capacitors.dat",
+               'T': "transistors.dat"}
+    circuit = [ ]
+    uniqueComp = [ ]
+
+    #find if the project ID exists in the projects.dat file
+    for item in rawData:
+        oldData = str(item).strip()
+        newData = str(oldData).split()
+        for item in newData:
+            if projectID == str(item):
+                circuit.append(newData[0])#take the circuit ID
+    #raise an error if it doesn't exists
+    if len(circuit) == 0:
+        raise ValueError("The projectID doesn't seems to exists")
+    #from the circuit ID, find the file in the circuits folder
+    for item in circuit:
+        IDFile = os.path.join(DataPath, 'circuits')
+        filename = 'circuit_' + str(item) + '.dat';
+        IDText = os.path.join(IDFile, filename)  # open projects file
+        with open(IDText) as f:
+            data = [line.rstrip('\n') for line in f.read().splitlines()]
+
+        compList = data[data.index('Components:')+2:]#get the components ID
+
+        CompText = os.path.join(DataFile, compMap[componentSymbol.upper()])#check dictionary to know which folder to open
+        with open(CompText) as f:
+            compData = [line.strip() for line in f.read().splitlines()]
+
+        for item1 in compList:
+            item1 = item1.strip()
+            for item2 in compData:
+                test = item2.split()
+                #print(item1)
+                if item1 in test[0].strip():
+                    if item1 in uniqueComp:#make sure the item is distinct
+                        del uniqueComp[uniqueComp.index(item1)]
+                    else:
+                        uniqueComp.append(item1)
+
+    return len(uniqueComp)
 
 def getComponentByStudent(studentName, componentSymbol):
     pass
@@ -46,4 +94,6 @@ def getCircuitByComponent(componentIDs):
 # This  block  is  optional
 if __name__  == "__main__":
 # Write  anything  here to test  your  code.
-    ...
+    num = getComponentCountByProject('082D6241-40EE-432E-A635-65EA8AA374B6', 'c')
+    print(num)
+    
