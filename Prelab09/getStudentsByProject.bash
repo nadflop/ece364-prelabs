1943975a8ce6a067bec9c540bd0c4a01f7f202fd
ee364g02 <mohdshah@purdue.edu>
2019-03-17 16:03:36 -0400

    second

--- Prelab09/getStudentsByProject.bash
+++ Prelab09/getStudentsByProject.bash
@@ -5,5 +5,21 @@
 # ID:     ee364g02
 # Date:   3/16/2019
 ########################################################
+base=~ee364/DataFolder/Prelab09
+ProjPath=${base}/maps/projects.dat
+CircPath=${base}/circuits
+StudPath=${base}/maps/students.dat
 
-DataPath=~ee364/DataFolder/Prelab09
+result=($(grep -h "$1" $ProjPath | cut -d' ' -f 5 | sort -u))
+length=${#result[@]}
+temp=()
+
+for ((i = 0; i != length; i++));
+do
+
+    circuit=$CircPath"/circuit_"${result[i]}".dat"
+    temp+=($(egrep -w "[0-9]+\-[0-9]+" $circuit))
+    
+done
+
+IFS=$'\n' ; echo "${temp[*]}" | grep -h "${temp[*]}" $StudPath | cut -d"|" -f1
