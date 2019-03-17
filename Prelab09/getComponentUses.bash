fccea55a19078e0f5afc480a62a28eb5531faae6
ee364g02 <mohdshah@purdue.edu>
2019-03-17 17:48:47 -0400

    five

--- Prelab09/getComponentUses.bash
+++ Prelab09/getComponentUses.bash
@@ -5,5 +5,18 @@
 # ID:     ee364g02
 # Date:   3/16/2019
 ########################################################
+base=~ee364/DataFolder/Prelab09
+ProjPath=${base}/maps/projects.dat
+CircPath=${base}/circuits
+StudPath=${base}/maps/students.dat
 
-DataPath=~ee364/DataFolder/Prelab09
+files=($(ls "$CircPath"))
+circ=()
+
+for i in "${files[@]}"
+do
+    circuit=$CircPath/$i
+    circ+=($(grep -l "$1" $circuit | cut -d'_' -f 2 | cut -d'.' -f 1))
+done
+
+echo "${#circ[@]}"
