05dfbf506efebdc344fb7fc4ee0068b17f3d1438
ee364g02 <mohdshah@purdue.edu>
2019-03-17 16:51:45 -0400

    three

--- Prelab09/getCircuitsByStudent.bash
+++ Prelab09/getCircuitsByStudent.bash
@@ -5,5 +5,17 @@
 # ID:     ee364g02
 # Date:   3/16/2019
 ########################################################
+base=~ee364/DataFolder/Prelab09
+ProjPath=${base}/maps/projects.dat
+CircPath=${base}/circuits
+StudPath=${base}/maps/students.dat
 
-DataPath=~ee364/DataFolder/Prelab09
+id=$(grep -h "$1" $StudPath | cut -d'|' -f2 | tr -d '[:space:]')
+
+files=($(ls "$CircPath"))
+
+for i in "${files[@]}"
+do
+    circuit=$CircPath/$i
+    grep -l "$id" $circuit | cut -d'_' -f 2 | cut -d'.' -f 1
+done
