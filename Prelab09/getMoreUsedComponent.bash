2faadd6249c898ef304b90ed1568bc388c53d7f2
ee364g02 <mohdshah@purdue.edu>
2019-03-17 17:54:23 -0400

    six

--- Prelab09/getMoreUsedComponent.bash
+++ Prelab09/getMoreUsedComponent.bash
@@ -5,5 +5,21 @@
 # ID:     ee364g02
 # Date:   3/16/2019
 ########################################################
+base=~ee364/DataFolder/Prelab09
+ProjPath=${base}/maps/projects.dat
+CircPath=${base}/circuits
+StudPath=${base}/maps/students.dat
 
-DataPath=~ee364/DataFolder/Prelab09
+files=($(ls "$CircPath"))
+circ1=()
+circ2=()
+
+for i in "${files[@]}"
+do
+    circuit=$CircPath/$i
+    circ1+=($(grep -l "$1" $circuit | cut -d'_' -f 2 | cut -d'.' -f 1))
+    circ2+=($(grep -l "$2" $circuit | cut -d'_' -f 2 | cut -d'.' -f 1))
+done
+
+
+if [[ "${#circ1[@]}" > "${#circ2[@]}"  ]]; then echo "$1"; else echo "$2"; fi
