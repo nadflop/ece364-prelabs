d199cb71a5329fe2ae2c2e2babb58370a2145b3f
ee364g02 <mohdshah@purdue.edu>
2019-03-16 23:14:11 -0400

    initial commit

--- Prelab09/getMoreUsedComponent.bash
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-########################################################
-# Author: Nur Nadhira Aqilah Binti Mohd Shah
-# Email:  mohdshah@purdue.edu
-# ID:     ee364g02
-# Date:   3/16/2019
-########################################################
-
-DataPath=~ee364/DataFolder/Prelab09
