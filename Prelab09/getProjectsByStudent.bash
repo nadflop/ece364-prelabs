0ab1b20a4715cda663c27302ab34ffd030f100cb
ee364g02 <mohdshah@purdue.edu>
2019-03-27 08:12:35 -0400

    fixed

--- Prelab09/getProjectsByStudent.bash
+++ Prelab09/getProjectsByStudent.bash
@@ -11,9 +11,9 @@ CircPath=${base}/circuits
 StudPath=${base}/maps/students.dat
 
 id=$(grep -h "$1" $StudPath | cut -d'|' -f2 | tr -d '[:space:]')
-circ=($(grep -l "$id" $CircPath/*.dat | cut -d'_' -f 2 | cut -d'.' -f 1))
+#circ=($(grep -l "$id" $CircPath/*.dat | cut -d'_' -f 2 | cut -d'.' -f 1))
 
-IFS=$'\n' ; echo "${circ[*]}" | grep -h "${circ[*]}" $ProjPath | tr -s ' ' | cut -d" " -f3 | sort -u
+#IFS=$'\n' ; echo "${circ[*]}" | grep -h "${circ[*]}" $ProjPath | tr -s ' ' | cut -d" " -f3 | sort -u
 
-IFS=$OLDIFS
-
+#IFS=$OLDIFS
+grep -h "$(grep -l "$id" $CircPath/*.dat | cut -d'_' -f 2 | cut -d'.' -f 1)" $ProjPath | tr -s ' ' | cut -d" " -f3 | sort -u
